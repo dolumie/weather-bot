67e0199847a7f4a0f0e18a9d47a30770ef79c090
fatAcake <164332760+fatAcake@users.noreply.github.com>
2025-04-16 16:15:25 +0700

    1.Создал эхл бота, как начальные шаги

--- Weather_bot/main.py
+++ Weather_bot/main.py
@@ -1,3 +1,14 @@
 from aiogram import Bot, Dispatcher
 from decouple import config
-from aiogram.client.default import DefaultBotProperties
+from bot.handlers import router
+import asyncio
+
+
+async def main():
+    bot = Bot(token=config('TELEGRAM_BOT_TOKEN'))
+    dp = Dispatcher()
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+if __name__=="__main__":
+    asyncio.run(main())
